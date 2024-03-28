222aca75fef7b2926523fc114ccb67210fd54fe9
CBarba12 <101232758+CBarba12@users.noreply.github.com>
2024-03-28 02:26:56 -0600

    prueba
    prueba

---
+++ project_progra/src/main/java/com/tcna/primeraweb/project_progra/ProjectPrograApplication.java
@@ -0,0 +1,13 @@
+package com.tcna.primeraweb.project_progra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPrograApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPrograApplication.class, args);
+    }
+
+}
---
+++ project_progra/src/test/java/com/tcna/primeraweb/project_progra/ProjectPrograApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcna.primeraweb.project_progra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPrograApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
