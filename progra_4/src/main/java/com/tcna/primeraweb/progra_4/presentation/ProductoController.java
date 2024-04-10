4b23fcbc0ffe1dcd9c7b0149be8aed0afd6e7fb4
joshua <joshyarit07@gmail.com>
2024-04-10 01:33:12 -0600

    Lo de productos hay un error a la hora de crear un producto de que no carga el id del proveedor

---
+++ progra_4/src/main/java/com/tcna/primeraweb/progra_4/presentation/ProductoController.java
@@ -0,0 +1,79 @@
+package com.tcna.primeraweb.progra_4.presentation;
+
+import com.tcna.primeraweb.progra_4.logic.ClienteEntity;
+import com.tcna.primeraweb.progra_4.logic.ProductoEntity;
+import com.tcna.primeraweb.progra_4.logic.ProveedorEntity;
+import com.tcna.primeraweb.progra_4.service.ClienteService;
+import com.tcna.primeraweb.progra_4.service.FacturaService;
+import com.tcna.primeraweb.progra_4.service.ProductoService;
+import com.tcna.primeraweb.progra_4.service.ProveedorService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/ProductoController")
+public class ProductoController {
+
+    @Autowired
+    private ProveedorService proveedorService;
+    @Autowired
+    private ClienteService clienteService;
+    @Autowired
+    private ProductoService productoService;
+    @Autowired
+    private FacturaService facturaService;
+
+
+
+
+
+
+    @GetMapping("/Listadeproductos") // Añade esta línea para mapear el método a la URL
+    public String listaProducto(Model model, HttpSession session){
+
+        List<ProductoEntity> productos=  productoService.ObtenerProductos();
+
+        // Verificar si hay clientes asociados al proveedor en la sesión
+        List<ClienteEntity> clientesProveedor = (List<ClienteEntity>) session.getAttribute("clientes");
+        if (clientesProveedor != null) {
+            // Si hay clientes asociados al proveedor en la sesión, agregarlos al modelo
+            model.addAttribute("clientesProveedor", clientesProveedor);
+        }
+        model.addAttribute("productos", productos);
+
+        return "listarProductos";
+    }
+
+    @GetMapping("/NuevoProducto") // Añade esta línea para mapear el método a la URL
+    public String nuevoProducto(Model model, HttpSession session){
+
+        List<ClienteEntity> clientesProveedor = (List<ClienteEntity>) session.getAttribute("clientes");
+        if (clientesProveedor != null) {
+            // Si hay clientes asociados al proveedor en la sesión, agregarlos al modelo
+            model.addAttribute("clientesProveedor", clientesProveedor);
+        }
+
+        model.addAttribute("producto", new ProductoEntity());
+        model.addAttribute("accion","/ProductoController/guardarProducto");
+
+        return "formularioProducto";
+    }
+
+    @PostMapping("/guardarProducto") // Añade esta línea para mapear el método a la URL
+    public String guardarProducto(@ModelAttribute ProductoEntity producto, HttpSession session){
+
+        String proveedorId = (String) session.getAttribute("proveedorId");
+
+        producto.setProveedor_id(proveedorId);
+
+        productoService.crearProductos(producto);
+
+        return "redirect:/ProductoController/Listadeproductos";
+    }
+
+}
---
+++ progra_4/src/main/java/com/tcna/primeraweb/progra_4/presentation/ProveedorController.java
@@ -0,0 +1,118 @@
+package com.tcna.primeraweb.progra_4.presentation;
+
+import com.tcna.primeraweb.progra_4.logic.ClienteEntity;
+import com.tcna.primeraweb.progra_4.logic.ProveedorEntity;
+import com.tcna.primeraweb.progra_4.service.ClienteService;
+import com.tcna.primeraweb.progra_4.service.ProveedorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/ProveedorController")
+public class ProveedorController {
+
+
+    @Autowired
+    private ProveedorService proveedorService;
+    @Autowired
+    private ClienteService clienteService;
+
+
+    @GetMapping("/Listadeproveedores") // Añade esta línea para mapear el método a la URL
+    public String listaProveedor(Model model){
+        List<ProveedorEntity> proveedores=  proveedorService.ObtenerProveedores();
+   
+        model.addAttribute("listaProveedor", proveedores);
+        return "listar";
+    }
+
+    @GetMapping("/nuevo")
+   public String MostrarFormularioNuevoProveedor(Model model){
+        model.addAttribute("proveedor",new ProveedorEntity());
+        model.addAttribute("accion","/ProveedorController/nuevo");
+        return "formulario";
+   }
+
+
+    @PostMapping("/nuevo")
+   public String guardarNuevoProveedor(@ModelAttribute ProveedorEntity proveedor){
+
+        proveedorService.crearProveedores(proveedor);
+
+        return "redirect:/ProveedorController/Listadeproveedores";
+   }
+
+
+
+    @GetMapping("/editar/{id}")
+    public String mostrarFormularioEditarPersona(@PathVariable String id, @ModelAttribute ProveedorEntity proveedor,Model model){
+
+        model.addAttribute("proveedor",proveedor);
+        model.addAttribute("accion","/ProveedorController/editar"+id);
+
+        return "formulario";
+    }
+
+
+
+    @PostMapping("/editar/{id}")
+    public String actualizarProveedores(@PathVariable String id, @ModelAttribute ProveedorEntity proveedor){
+     proveedorService.actualizarProveedro(id,proveedor);
+
+     return "redirect:/ProveedorController/Listadeproveedores";
+    }
+
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminarProveedor(@PathVariable String id, Model model){
+        try {
+            proveedorService.eliminarProveedor(id);
+            return "redirect:/ProveedorController/Listadeproveedores";
+        } catch (DataIntegrityViolationException e) {
+            // Captura la excepción y agrega un mensaje de error al modelo
+            model.addAttribute("error", "No se puede eliminar este proveedor porque hay clientes asociados a él.");
+            return "redirect:/ProveedorController/Listadeproveedores";
+        }
+    }
+
+
+
+
+    @PostMapping("/crear-cliente")
+    public String crearCliente(@RequestParam("nombre") String nombre,
+                               @RequestParam("direccion") String direccion,
+                               @RequestParam("correoElectronico") String correoElectronico,
+                               @RequestParam("proveedorId") String proveedorId) {
+
+        // Obtener el proveedor asociado al ID proporcionado
+        ProveedorEntity proveedor = proveedorService.obtenerProveedorPorId(proveedorId);
+
+        // Verificar si el proveedor existe
+        if (proveedor != null) {
+            // Crear una nueva instancia de ClienteEntity
+            ClienteEntity nuevoCliente = new ClienteEntity();
+            nuevoCliente.setNombre(nombre);
+            nuevoCliente.setDireccion(direccion);
+            nuevoCliente.setCorreo_electronico(correoElectronico);
+            // Establecer la relación con el proveedor
+            nuevoCliente.setProveedor_id(proveedor.getIdProveedor());
+
+            // Guardar el cliente en la base de datos
+            clienteService.crearCliente(nuevoCliente);
+
+            // Redireccionar a la página de éxito o a donde desees
+            return "redirect:/cliente-creado";
+        } else {
+            // Manejar el caso en el que el proveedor no exista
+            return "redirect:/error-crear-cliente";
+        }
+    }
+
+
+
+}
