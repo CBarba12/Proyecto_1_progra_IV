ada214ec97c03060325ecc71a52e59334c7e0446
joshua <joshyarit07@gmail.com>
2024-04-11 00:22:24 -0600

    pagina que sigue despues del login - Clientes (listo) - Productos (listo) - Facturas (Medio listo)

--- progra_4/src/main/java/com/tcna/primeraweb/progra_4/presentation/FacturaController.java
+++ progra_4/src/main/java/com/tcna/primeraweb/progra_4/presentation/FacturaController.java
@@ -1,12 +1,21 @@
 package com.tcna.primeraweb.progra_4.presentation;
 
+import com.tcna.primeraweb.progra_4.logic.ClienteEntity;
+import com.tcna.primeraweb.progra_4.logic.FacturaEntity;
+import com.tcna.primeraweb.progra_4.logic.ProductoEntity;
 import com.tcna.primeraweb.progra_4.service.ClienteService;
 import com.tcna.primeraweb.progra_4.service.FacturaService;
 import com.tcna.primeraweb.progra_4.service.ProductoService;
 import com.tcna.primeraweb.progra_4.service.ProveedorService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/FacturaController")
@@ -19,4 +28,42 @@ public class FacturaController {
     private ProductoService productoService;
     @Autowired
     private FacturaService facturaService;
+
+
+    @GetMapping("/ListadeFacturas") // Añade esta línea para mapear el método a la URL
+    public String listarFacturas(Model model, HttpSession session) {
+        // Verificar si hay facturas asociadas al proveedor en la sesión
+        List<ClienteEntity> clientesProveedor = (List<ClienteEntity>) session.getAttribute("clientes");
+        if (clientesProveedor != null) {
+            // Si hay clientes asociados al proveedor en la sesión, agregarlos al modelo
+            model.addAttribute("clientesProveedor", clientesProveedor);
+        }
+        List<FacturaEntity> facturasProveedor = (List<FacturaEntity>) session.getAttribute("facturas");
+        if (facturasProveedor != null) {
+            // Si hay facturas asociadas al proveedor en la sesión, agregarlos al modelo
+            model.addAttribute("facturasProveedor", facturasProveedor);
+        }
+        // Obtener todas las facturas y agregarlos al modelo
+        List<FacturaEntity> facturas = facturaService.ObtenerFacturas();
+        model.addAttribute("facturas", facturas);
+        return "listarFacturas"; // Suponiendo que el nombre de la vista es listarFacturas.html
+    }
+    @GetMapping("/NuevaFactura") // Añade esta línea para mapear el método a la URL
+    public String nuevaFactura(Model model, HttpSession session) {
+        List<ClienteEntity> clientesProveedor = (List<ClienteEntity>) session.getAttribute("clientes");
+        if (clientesProveedor != null) {
+            // Si hay clientes asociados al proveedor en la sesión, agregarlos al modelo
+            model.addAttribute("clientesProveedor", clientesProveedor);
+        }
+        model.addAttribute("factura", new FacturaEntity());
+        model.addAttribute("accion","/FacturaController/guardarFactura");
+        return "formularioFactura";
+    }
+
+    @PostMapping("/guardarFactura") // Añade esta línea para mapear el método a la URL
+    public String guardarFactura(FacturaEntity factura, HttpSession session) {
+        facturaService.crearFactura(factura);
+        return "redirect:/FacturaController/ListadeFacturas"; // Redirigir a la lista de facturas
+    }
+
 }
