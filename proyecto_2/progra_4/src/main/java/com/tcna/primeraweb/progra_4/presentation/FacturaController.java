04a1547eb855f665f40c90e642b7f0f97413fce8
joshua <joshyarit07@gmail.com>
2024-04-13 11:46:00 -0600

    Facturacion listo Falta el diseño y pdf y xml

--- proyecto_2/progra_4/src/main/java/com/tcna/primeraweb/progra_4/presentation/FacturaController.java
+++ proyecto_2/progra_4/src/main/java/com/tcna/primeraweb/progra_4/presentation/FacturaController.java
@@ -14,8 +14,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Controller
@@ -38,9 +42,22 @@ public class FacturaController {
         List<FacturaEntity> filteredFacturas = facturaEntities.stream()
                 .filter(factura -> factura.getProveedor().equals(ID))
                 .collect(Collectors.toList());
+        List<ProductoEntity> productos = productoService.obtenerProductoPorProveedor(ID);
 
-        if (!filteredFacturas.isEmpty()) {
+
+        // Crear un mapa de nombres de productos
+       Map<String, String> nombresProductos = productos.stream()
+        .collect(Collectors.toMap(producto -> String.valueOf(producto.getProductoId()), ProductoEntity::getNombre));
+
+        // Crear un mapa de nombres de productos
+        Map<String, Double> preciosProductos = productos.stream()
+                .collect(Collectors.toMap(producto -> String.valueOf(producto.getProductoId()), ProductoEntity::getPrecio));
+
+        if (!filteredFacturas.isEmpty() && !productos.isEmpty()) {
             model.addAttribute("Facturas", filteredFacturas);
+            model.addAttribute("Productos", productos);
+            model.addAttribute("nombresProductos", nombresProductos);
+            model.addAttribute("preciosProductos", preciosProductos);
         }
 
         return "listarFacturas"; // Suponiendo que el nombre de la vista es listarFacturas.html
@@ -66,10 +83,34 @@ public class FacturaController {
     }
 
     @PostMapping("/guardarFactura") // Añade esta línea para mapear el método a la URL
-    public String guardarFactura(FacturaEntity factura, HttpSession session) {
+    public String guardarFactura(@RequestParam("clienteId") String clienteId, @RequestParam("productoId") String productoId, @RequestParam("cantidad") String cantidad, Model model, HttpSession session) {
+        String ID= (String) session.getAttribute("id_proveedor");
+        List<ProductoEntity> productos = productoService.obtenerProductoPorProveedor(ID);
+        List<ClienteEntity> clientesProveedor = clienteService.obtenerClientesPorProveedor(ID);
 
+        ProductoEntity producto = productos.stream()
+            .filter(p -> p.getProductoId() == Integer.parseInt(productoId))
+            .findFirst()
+            .orElse(null);
 
+        ClienteEntity cliente = clientesProveedor.stream()
+            .filter(c -> c.getClienteId().equals(clienteId))
+            .findFirst()
+            .orElse(null);
 
+        if (producto != null && cliente != null) {
+            FacturaEntity factura = new FacturaEntity();
+            LocalDate fecha = LocalDate.now();
+            factura.setProveedor(ID);
+            factura.setCliente(cliente.getClienteId());
+            factura.setId_producto(producto.getProductoId());
+            factura.setFecha(Date.valueOf(fecha));
+            factura.setCantidad(Integer.parseInt(cantidad));
+            factura.setTotal(producto.getPrecio() * factura.getCantidad());
+            facturaService.crearFactura(factura);
+        }
+
+        model.addAttribute("id_proveedor",ID);
         return "redirect:/FacturaController/ListadeFacturas"; // Redirigir a la lista de facturas
     }
 
