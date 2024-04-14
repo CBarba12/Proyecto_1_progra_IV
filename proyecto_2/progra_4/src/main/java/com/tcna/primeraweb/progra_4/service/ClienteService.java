87eddf4554314776bb6a0223dd9a301d3cd26877
Angélica <eolica55@gmail.com>
2024-04-13 21:32:24 -0600

    Implementacion de la funcionalidad agregarClientes

--- proyecto_2/progra_4/src/main/java/com/tcna/primeraweb/progra_4/service/ClienteService.java
+++ proyecto_2/progra_4/src/main/java/com/tcna/primeraweb/progra_4/service/ClienteService.java
@@ -41,6 +41,13 @@ public class ClienteService {
     public List<ClienteEntity> obtenerClientesPorProveedor(String id_proveedor) {
         return  clienteRepository.findByProveedorId(id_proveedor);
     }
+    public void actualizarCliente(ClienteEntity cliente) {
+        clienteRepository.save(cliente);
+    }
+
+    public void eliminarCliente(String id) {
+        clienteRepository.deleteById(id);
+    }
 
 
 }
