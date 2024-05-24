7ae89e78fcf17c11ae277ef75dc8149169afea1a
Joshua <joshyarit07@gmail.com>
2024-05-24 11:00:34 -0600

    ProveedorController cambios

--- proyecto_2/progra_4/src/main/java/com/tcna/primeraweb/progra_4/presentation/ProveedorController.java
+++ proyecto_2/progra_4/src/main/java/com/tcna/primeraweb/progra_4/presentation/ProveedorController.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.tcna.primeraweb.progra_4.service.HaciendaStub;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -33,58 +34,54 @@ public class ProveedorController {
 
 
     @GetMapping("/Listadeproveedores") // Añade esta línea para mapear el método a la URL
-    public String listaProveedor(Model model, HttpSession session) {
-        String estado[] = {"Aceptado","Rechazado"};
-
+    public List<ProveedorEntity> listaProveedor(Model model, HttpSession session) {
       List<ProveedorEntity> proveedores=  proveedorService.ObtenerProveedores();
+      ProveedorEntity proveedor = proveedorService.obtenerProveedorPorId((String) session.getAttribute("id_admin"));
+      // Define el orden de los estados
+      Map<String, Integer> estadoOrden = new HashMap<>();
+      estadoOrden.put("Aceptado", 2);
+      estadoOrden.put("Rechazado", 3);
+      estadoOrden.put("En espera", 1);
 
-        ProveedorEntity proveedor = proveedorService.obtenerProveedorPorId((String) session.getAttribute("id_admin"));
-
-
-
-        // Define el orden de los estados
-        Map<String, Integer> estadoOrden = new HashMap<>();
-        estadoOrden.put("Aceptado", 2);
-        estadoOrden.put("Rechazado", 3);
-        estadoOrden.put("En espera", 1);
-
-        // Ordena la lista de proveedores según el estado
-        proveedores.sort((p1, p2) -> {
-            int orden1 = estadoOrden.getOrDefault(p1.getEstado(), 0);
-            int orden2 = estadoOrden.getOrDefault(p2.getEstado(), 0);
-            return Integer.compare(orden1, orden2);
-        });
-
-        model.addAttribute("est",estado);
-        model.addAttribute("listaProveedor", proveedores);
-
-      if (proveedor.getAdmin()==1) {
-          return "listarproveedor";
-        }else {
-            return "index";
-        }
-
+      // Ordena la lista de proveedores según el estado
+      proveedores.sort((p1, p2) -> {
+        int orden1 = estadoOrden.getOrDefault(p1.getEstado(), 0);
+        int orden2 = estadoOrden.getOrDefault(p2.getEstado(), 0);
+        return Integer.compare(orden1, orden2);
+      });
+      return proveedores;
     }
 
+    @PostMapping("NewProveedor")
+    public ResponseEntity<ProveedorEntity> crearProveedor(@RequestBody ProveedorEntity proveedor) {
+        try {
+            proveedor.setAdmin((byte) 0);
+            proveedor.setEstado("En espera");
+            proveedorService.crearProveedores(proveedor);
+            return ResponseEntity.ok(proveedor);
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
 
+// Falta
     @GetMapping("estado/{id}/{estado}")
-    public String cambiarEstado(@PathVariable("id") String id, @PathVariable("estado") String estado, Model model, HttpSession session) {
-        String ID = (String) session.getAttribute("id_proveedor");
-        ProveedorEntity proveedor = proveedorService.obtenerProveedorPorId(id);
+    public ResponseEntity<ProveedorEntity> cambiarEstado(@RequestBody ProveedorEntity p, @PathVariable String id, @PathVariable String estado) {
+        ProveedorEntity proveedor = proveedorService.obtenerProveedorPorId(p.getIdProveedor());
         if(estado.equals("Aceptado")){
             proveedor.setEstado("Aceptado");
             proveedorService.actualizarProveedor(id,proveedor);
-
+            return ResponseEntity.ok(proveedor);
         }else if(estado.equals("Rechazado")){
             proveedor.setEstado("Rechazado");
             proveedorService.actualizarProveedor(id,proveedor);
+            return ResponseEntity.ok(proveedor);
         }
-
-        return "redirect:/ProveedorController/Listadeproveedores";
+        return ResponseEntity.badRequest().build();
     }
 
 
-
+//Revisar
     @GetMapping("/nuevo")
    public String MostrarFormularioNuevoProveedor(Model model){
         model.addAttribute("proveedor",new ProveedorEntity());
@@ -92,7 +89,7 @@ public class ProveedorController {
    }
 
 
-
+// Arreglar el registro de proveedores
     @PostMapping("/nuevo_proveedor")
     public void guardarNuevoProveedor(@RequestBody ProveedorEntity proveedor){
 
@@ -144,6 +141,7 @@ public class ProveedorController {
 
 
 //----------------------------------------------------------------------------------------
+    //Revisar
     @GetMapping("/editar/{id}")
     public String mostrarFormularioEditarPersona(@PathVariable String id, @ModelAttribute ProveedorEntity proveedor,Model model, HttpSession session){
         String ID = (String) session.getAttribute("id_proveedor");
@@ -158,7 +156,7 @@ public class ProveedorController {
 
 
 
-
+    // Revisar
     @PostMapping("/editar/{id}")
     public String actualizarProveedores(@PathVariable String id, @ModelAttribute ProveedorEntity proveedor, HttpSession session){
         String ID = (String) session.getAttribute("id_proveedor");
@@ -175,7 +173,7 @@ public class ProveedorController {
 
     //---------------------------------------------------------------
 
-
+    // Revisar
     @PostMapping("/crear-cliente")
     public String crearCliente(@RequestParam("nombre") String nombre,
                                @RequestParam("direccion") String direccion,
